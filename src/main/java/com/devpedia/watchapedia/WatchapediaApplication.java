92d6b1111e952b8329d5cb5e38500e87f7a76dd4
kdpark <wert1229@gmail.com>
2020-11-02 22:50:33 +0900

    Init

---
+++ src/main/java/com/devpedia/watchapedia/WatchapediaApplication.java
@@ -0,0 +1,13 @@
+package com.devpedia.watchapedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchapediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchapediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpedia/watchapedia/WatchapediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpedia.watchapedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchapediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
